50fdd36e16df95126baaf47ad504ac83b12664d3
Micheal A <aad3rinto@gmail.com>
2023-04-16 10:35:19 +0000

    revert, not working

--- src/index.js
+++ src/index.js
@@ -6,3 +6,39 @@
 //E.g.
 //Created by Angela Yu.
 //Copyright 2019.
+
+// import React from "react"
+// import ReactDOM from "react-dom";
+// const React = require("react");
+// const ReactDOM = require("react-dom");
+
+
+
+// const NAME = "Ade Aderinto";
+// const YEAR = new Date().getFullYear();
+
+
+
+// ReactDOM.render(
+// <div>
+// <p>Created by {NAME} </p>
+// <p>Copyright {YEAR} </p>
+
+
+// </div>,
+// document.getElementById("root"))
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const name = "Derick Howler";
+const currentYear = New(Date())
+
+ReactDOM.render(
+    <div>
+        <P>{name}</P>
+        <p>{currentYear}</p>
+    </div>,
+    document.getElementById('root')
+)
